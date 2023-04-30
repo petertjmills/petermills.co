e23390bece0fac4fce2bb3742ee5fc76c8318888
petertjmills <56031935+petertjmills@users.noreply.github.com>
2023-04-30 16:06:57 +0100

    Was it env variables the whole fucking time

--- src/utils/contentful.ts
+++ src/utils/contentful.ts
@@ -1,9 +1,15 @@
 import contentful from "contentful";
 
+if (import.meta.env.DEV){
+    process.env.CONTENTFUL_SPACE_ID = import.meta.env.CONTENTFUL_SPACE_ID;
+    process.env.CONTENTFUL_DELIVERY_TOKEN = import.meta.env.CONTENTFUL_DELIVERY_TOKEN;
+    process.env.CONTENTFUL_PREVIEW_TOKEN = import.meta.env.CONTENTFUL_PREVIEW_TOKEN;
+}
+
 export const contentfulClient = contentful.createClient({
-  space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.DEV
+    ? process.env.CONTENTFUL_PREVIEW_TOKEN
+    : process.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: process.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
